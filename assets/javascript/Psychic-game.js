3f2c4070d59f0b0f401751d9efc19547d3e0545f
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-22 14:11:17 -0400

    linking js file correctly

--- assets/javascript/Psychic-game.js
+++ assets/javascript/Psychic-game.js
@@ -1,4 +1,4 @@
-var Numbers = ["1", "2", "3", "4, "5", "6", "7", "8", "9", "10"];
+var Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 console.log (Numbers)
 
 for (var i = 0; i < Numbers.length; i++) {
